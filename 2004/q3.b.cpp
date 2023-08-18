fe22477921af7780428f4c8327afc6dcb19b3f28
Seyoon Park <114111012+s3yoonpark@users.noreply.github.com>
2023-08-18 22:55:15 +0100

    2004 & 2014 q3's

---
+++ 2004/q3.b.cpp
@@ -0,0 +1,68 @@
+#include <iostream> 
+using namespace std;
+
+string morse[26];
+
+void process(int index, string tmp, int& cnt, string& morsed) {
+	if (index == (int)morsed.length()) {
+		// finished processing 
+		//cout << tmp << '\n'; 
+		++cnt; 
+		return; 
+	}
+	for (int i = 1; i <= min(4, (int)morsed.length() - index); ++i) {
+		int match = -1; 
+		string t = morsed.substr(index, i); 
+		for (int j = 0; j < 26 && match == -1; ++j) {
+			if (t == morse[j]) {
+				match = j; 
+			}
+		}
+		if (match != -1) {
+			process(index+i, tmp + (char)('a'+match), cnt, morsed); 
+		}
+	}
+	return; 
+}
+
+int solve(string t) {
+	string morsed = t; 
+	int cnt = 0; 
+	process(0, "", cnt, morsed); 
+	return cnt; 
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0); 
+	morse[0] = ".-"; 
+	morse[1] = "-..."; 
+	morse[2] = "-.-."; 
+	morse[3] = "-.."; 
+	morse[4] = "."; 
+	morse[5] = "..-."; 
+	morse[6] = "--."; 
+	morse[7] = "...."; 
+	morse[8] = ".."; 
+	morse[9] = ".---"; 
+	morse[10] = "-.-"; 
+	morse[11] = ".-.."; 
+	morse[12] = "--"; 
+	morse[13] = "-."; 
+	morse[14] = "---"; 
+	morse[15] = ".--."; 
+	morse[16] = "--.-"; 
+	morse[17] = ".-."; 
+	morse[18] = "..."; 
+	morse[19] = "-"; 
+	morse[20] = "..-"; 
+	morse[21] = "...-"; 
+	morse[22] = ".--"; 
+	morse[23] = "-..-"; 
+	morse[24] = "-.--"; 
+	morse[25] = "--.."; 
+	cout << solve("-----") << '\n'; 
+	cout << solve("-..-----.") << '\n';  
+
+	return 0; 
+}
--- 2014/q3.cpp
+++ 2014/q3.cpp
@@ -1,52 +1,64 @@
-#include <bits/stdc++.h>
+#include <iostream> 
 using namespace std;
 
 typedef unsigned long long ull; 
 
-ull n;  
-string lst = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; 
+string NAME = "Seyoon Park", SCHOOL = "Harrow School"; 
 
-ull dp[37][36];
+const int mxL = 36; 
+const string LIST = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; 
+ull dp[mxL+1][mxL+1]; 
+ull n; 
 
-void solve() {
-	string ans = ""; 
-	ull l = 1; 
-	while (1) {
-		//bool added = 0; 
-		for (char c : lst) {
-			// starting letter 
-			ull t = dp[l][lst.find(c)]; 
-			if (n > t) {
-				n -= t; 
-			} else {
-				ans += c; 
-				//added = 1; 	
-				while (ans.length() != l) {
-					
-				}
-				break; 
+void init() {
+	for (int j = 1; j <= mxL; ++j) dp[1][j] = 1; 
+
+	for (int i = 2; i <= mxL; ++i) {
+		for (int j = 1; j <= mxL; ++j) {
+			for (int k = j+1; k <= mxL; ++k) {
+				dp[i][j] += dp[i-1][k]; 
 			}
 		}
-		//if (!added) ++l; 
-		if (!n) break; 
 	}
-	cout << ans << '\n'; 
-
-	return; 
 }
 
-int main() {
-	for (int i = 0; i < 36; ++i) dp[1][i] = 1; 	
-	for (int i = 2; i <= 36; ++i) {
-		for (int j = 0; j < 36; ++j) {
-			for (int k = j+1; k < 36; ++k) {
-				dp[i][j] += dp[i-1][k]; 
-
+string solve() {
+	string ans = ""; 
+	bool added = 0; 
+	for (int i = 1; i <= mxL && !added; ++i) {
+		for (int j = 1; j <= mxL && !added; ++j) {
+			// length i starting with j 
+			if (n > dp[i][j]) {
+				n -= dp[i][j]; 
+			} else {
+				// found the correct one 
+				added = 1; 
+				int c = j; 
+				ans += LIST[c-1]; 
+				while ((int)ans.length() != i) {
+					for (int k = c+1; k <= mxL; ++k) {
+						if (n > dp[i-(int)ans.length()][k]) {
+							n -= dp[i-(int)ans.length()][k]; 
+						} else {
+							c = k; 
+							ans += LIST[k-1]; 
+							break; 
+						}
+					}
+				}
 			}
 		}
 	}
+	return ans; 
+}
+ 
+int main() {
+	cout << "NAME : " << NAME << '\n'; 	
+	cout << "SCHOOL : " << SCHOOL << '\n'; 
+
+	init(); 
 	cin >> n; 
-	solve(); 
+	cout << solve() << '\n'; 
 
 	return 0; 
 }
