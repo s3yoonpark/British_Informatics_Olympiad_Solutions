21f4dfa3b30b15e3f5cfb6f2ab4be45594db815c
Seyoon Park <114111012+s3yoonpark@users.noreply.github.com>
2023-12-04 00:11:58 +0000

    Update q2.a.cpp

--- 2017/q2.a.cpp
+++ 2017/q2.a.cpp
@@ -1,135 +1,129 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h> 
+using namespace std; 
 
-int a, p; 
-int b, q;
-int t; 
-
-char sq[5][5]; 
-bool path[37][37]; 
-int clockwise[4] = {-6, 1, 6, -1}; 
-int anticlockwise[4] = {-6, -1, 6, 1}; 
-
-bool check(int n) {
-	return n >= 1 && n <= 36; 
-}
-
-bool eval(int player) {
-	char symbol; 
-	if (player == 1) symbol = 'X';
-	else symbol = 'O'; 
-	bool changed = 0; 
-	for (int i = 1; i <= 36; ++i) {
-		if (sq[(i-i%5)/5][i%5-1] != '*') continue; 
-		int c = i; 
-		int d = i+1; 
-		int e = i+7; 
-		int f = i+6; 
-		if (check(c) && check(d) && check(e) && check(f)) {
-			if (path[c][d] && path[d][e] && path[e][f] && path[f][c]) {
-				sq[(i-i%5)/5][i%5-1] = symbol; 
-				changed = 1; 
-			}
-		}		
-	}
-	return changed; 
-}
-
-void amove() {
-	cout << 1 << '\n'; 
-	a += p; 
-	a %= 36; 
-	if (!a) a += 36; 
-	bool joined = 0; 
-	while (!joined) {
-		for (int j = 0; j < 4; ++j) {
-			if (check(a+clockwise[j])) {
-				if (path[a][a+clockwise[j]] || path[a+clockwise[j]][a]) {
-					continue; 	
-				} else {
-					path[a][a+clockwise[j]] = 1;
-					path[a+clockwise[j]][a] = 1; 
-					joined = 1; 
-					break; 
-				}
-			}
-		}
-		if (!joined) {
-			++a; 
-			a %= 36; 
-			if (!a) a += 36; 
-		}	
-	}
-	if (eval(1)) amove(); 
-	return; 
-}
-
-void bmove() {
-	cout << 2 << '\n'; 
-	b += q; 
-	b %= 36; 
-	if (!b) b += 36; 
-	// clockwise 
-	bool joined = 0; 
-	while (!joined) {
-		for (int j = 0; j < 4; ++j) {
-			if (check(b+anticlockwise[j])) {
-				if (path[b][b+anticlockwise[j]] || path[b+anticlockwise[j]][a]) {
-					continue; 	
-				} else {
-					path[b][b+anticlockwise[j]] = 1;
-					path[b+anticlockwise[j]][b] = 1; 
-					joined = 1; 
-					break; 
-				}
-			}
-		}
-		if (!joined) {
-			++b; 	
-			b %= 36; 
-			if (!b) b += 36; 
-		}
-	}
-	if (eval(2)) bmove(); 
-	return; 
-}
-
-void solve() {
-	for (int i = 0; i < t; ++i) {
-		amove(); 	
-		bmove(); 
-	}	
-	cout << "HERE" << '\n'; 
-	for (int i = 1; i <= 36; ++i) {
-		for (int j = 1; j <= 36; ++j) {
-			cout << i << ' ' << j << ' ' << path[i][j] << '\n'; 
-		}
-	}
-	cout << "HERE" << '\n'; 
-	int ac = 0, bc = 0; 
-	for (int i = 0; i < 5; ++i) {
-		for (int j = 0; j < 5; ++j) {
-			cout << sq[i][j] << ' ';
-			if (sq[i][j] == 'X') {
-				++ac; 
-			}
-			if (sq[i][j] == 'O') {
-				++bc; 
-			}
-		}
-		cout << '\n'; 
-	}
-	cout << ac << ' ' << bc << '\n'; 
-	return; 
-}
+bool grid[36][36]; 
+char ans[36]; 
+bool again=0; 
 
 int main() {
-	//ios_base::sync_with_stdio(0); 
-	//cin.tie(0); 
-	memset(path, 0, sizeof(path)); 
-	memset(sq, '*', sizeof(sq)); 
-	cin >> a >> p >> b >> q >> t;
-	solve(); 
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0); 
+
+	for (int i=0; i<36; ++i) ans[i]='*';
+
+	int p1, m1, p2, m2, t;
+	cin>>p1>>m1>>p2>>m2>>t; 
+	--p1, --p2; 
+	int turn=0; 
+	for (int i=1; i<=t; ++i) {
+		//cout<<'\n';
+		//cout<<i<<' '<<turn<<'\n';
+		if (turn==0) {
+			bool played=0; 
+			int cnt=0; 
+			while (!played) {
+				if (!cnt) {p1+=m1; p1%=36;} 
+				else {p1=(p1+1)%36;} 
+				++cnt;
+				for (int x : {-6, 1, 6, -1}) {
+					if (x==-6) {
+						if (p1-6<0) continue; 	
+					}
+					if (x==1) {
+						if (p1%6==5) continue; 
+					}
+					if (x==6) {
+						if (p1+6>35) continue; 	
+					}
+					if (x==-1) {
+						if (p1%6==0) continue; 
+					}
+					int n=p1+x; 	
+					if (n>=0&&n<36&&!grid[p1][n]&&!grid[n][p1]) {
+						//cout<<p1<<' '<<n<<'\n';
+						grid[p1][n]=1; 
+						grid[n][p1]=1; 
+						// now check if that completed a square 
+						for (int a=0; a<36; ++a) {
+							if (ans[a]!='*') continue; 
+							if (a%6==5) continue; 
+							if (a+6>35) continue; 
+							if ((a+6)%6==5) continue; 
+							if (a+7>35) continue; 
+							if (grid[a][a+1]&&grid[a][a+6]&&grid[a+6][a+7]&&grid[a+1][a+7]) {
+								// completed a square!
+								ans[a]='X';
+								again=1; 
+							}
+						}
+						played=1; 
+						break; 
+					}
+				}
+			}
+		} else {
+			bool played=0; 
+			int cnt=0;  
+			while (!played) {
+				if (!cnt) {p2+=m2; p2%=36;} 
+				else p2=(p2+1)%36; 
+				++cnt; 
+				for (int x : {-6, -1, 6, 1}) {
+					if (x==-6) {
+						if (p2-6<0) continue; 	
+					}
+					if (x==1) {
+						if (p2%6==5) continue; 
+					}
+					if (x==6) {
+						if (p2+6>35) continue; 	
+					}
+					if (x==-1) {
+						if (p2%6==0) continue; 
+					}
+					int n=p2+x; 	
+					if (n>=0&&n<36&&!grid[p2][n]&&!grid[n][p2]) {
+						//cout<<p2<<' '<<n<<'\n';
+						grid[p2][n]=1; 
+						grid[n][p2]=1; 
+						// now check if that completed a square 
+						for (int a=0; a<36; ++a) {
+							if (ans[a]!='*') continue; 
+							if (a%6==5) continue; 
+							if (a+6>35) continue; 
+							if ((a+6)%6==5) continue; 
+							if (a+7>35) continue; 
+							if (grid[a][a+1]&&grid[a][a+6]&&grid[a+6][a+7]&&grid[a+1][a+7]) {
+								// completed a square!
+								ans[a]='O';
+								again=1; 
+							}
+						}
+						played=1; 
+						break; 
+					}
+				}
+			}
+
+		}
+		if (!again) {
+			turn=!turn; 
+		} else {
+			again=0; 
+		}
+	}
+	int pt0=0, pt1=0; 
+	for (int i=0; i<36; ++i) {
+		if (i==30) break; 
+		if (i%6==5) {
+			cout<<'\n';
+			continue; 
+		}
+		cout<<ans[i]<<' ';
+		if (ans[i]=='X') ++pt0; 
+		if (ans[i]=='O') ++pt1; 
+	}
+	cout<<pt0<<' '<<pt1<<'\n';
 
 	return 0; 
 }
